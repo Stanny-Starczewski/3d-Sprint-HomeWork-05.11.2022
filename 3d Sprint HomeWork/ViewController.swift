2371fc316c2c8df0b6b696dc31650476f0163335
Stanny <113798428+Stanny-Starczewski@users.noreply.github.com>
2022-11-05 22:30:22 +0300

    3d Sprint HomeWork
    3d Sprint HomeWork

--- 3d Sprint HomeWork/ViewController.swift
+++ 3d Sprint HomeWork/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var Button: UIButton!
+    private var score: Int = 0 {
+        didSet {
+            updateLabelText()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func buttunDidUp(_ sender: Any) {
+        score += 1
+    }
+    private func updateLabelText() {
+        Label.text = "\(score)"
+    }
 }
 
