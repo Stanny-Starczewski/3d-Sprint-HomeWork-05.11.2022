5e863dc354edcbfa1c93e0845bafded5aa312ff3
Stanislav Orel <spaceboyua@yahoo.com>
2022-11-05 19:06:46 +0300

    Initial Commit

---
+++ 3d Sprint HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3d Sprint HomeWork
+//
+//  Created by Gennadii Kulikov on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 3d Sprint HomeWorkUITests/_d_Sprint_HomeWorkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _d_Sprint_HomeWorkUITestsLaunchTests.swift
+//  3d Sprint HomeWorkUITests
+//
+//  Created by Gennadii Kulikov on 05.11.2022.
+//
+
+import XCTest
+
+final class _d_Sprint_HomeWorkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
